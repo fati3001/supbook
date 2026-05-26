31bb2f5fd9b54465feff8a896eec094105ab3f8d
fatimatou Diallo <fatimatou.diallo@supinfo.com>
2026-05-26 14:49:13 +0200

    feat: setup strapi content types and permissions

---
+++ backend/src/api/library-collection/routes/library-collection.ts
@@ -0,0 +1,7 @@
+/**
+ * library-collection router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::library-collection.library-collection');
---
+++ backend/src/api/library-collection/services/library-collection.ts
@@ -0,0 +1,7 @@
+/**
+ * library-collection service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::library-collection.library-collection');
